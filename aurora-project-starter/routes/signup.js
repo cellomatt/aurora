3eaefc4d8b6fafa8d5a54881222813baf26df333
Max Lettenberger <maxlettenberger@gmail.com>
2021-01-04 15:23:18 -0800

    Added error validation for signup form

--- aurora-project-starter/routes/signup.js
+++ aurora-project-starter/routes/signup.js
@@ -3,32 +3,94 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const csrf = require("csurf");
 const db = require("../db/models");
-const { User } = db;
-const { asyncHandler } = require("../utils/utils");
+const {
+  User
+} = db;
+const {
+  asyncHandler,
+  handleValidationErrors
+} = require("../utils/utils");
 
-const csrfProtection = csrf({ cookie: true });
-router.use(express.urlencoded({ extended: false }));
+const csrfProtection = csrf({
+  cookie: true
+});
+router.use(express.urlencoded({
+  extended: false
+}));
 router.use(express.json());
+const {
+  check,
+  validationResult
+} = require('express-validator');
+
+const formValidators = [
+  check("username")
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage("Please enter a username.")
+  .isLength({
+    max: 15
+  })
+  .withMessage("Must be 15 characters or less"),
+  check("email")
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage("Please enter an email.")
+  .isEmail()
+  .withMessage("Must follow common email format, i.e., something@mail.com")
+  .isLength({
+    max: 60
+  })
+  .withMessage("Must be 60 characters or less"),
+  check("password")
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage("Please enter a password.")
+  .isLength({
+    min: 7
+  })
+  .withMessage("Must have at least 7 characters")
+  .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/)
+  .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")')
+];
 
 /* GET sign-up form. */
 router.get("/", csrfProtection, function (req, res, next) {
-  res.render("signup", { csrfToken: req.csrfToken() });
+  res.render("signup", {
+    csrfToken: req.csrfToken()
+  });
 });
 
 router.post(
   "/",
   csrfProtection,
+  formValidators,
   asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body;
-    // check if user already exists in db
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({
-      username,
-      email,
-      hashedPassword,
-    });
 
-    res.redirect("/");
+    const validationErrors = validationResult(req);
+
+    if (validationErrors.isEmpty()) {
+      const {
+        username,
+        email,
+        password
+      } = req.body;
+      // check if user already exists in db
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword,
+      });
+
+      res.redirect("/");
+    } else {
+      const errors = validationErrors.array().map((err) => err.msg);
+      console.log(errors);
+    }
   })
 );
 

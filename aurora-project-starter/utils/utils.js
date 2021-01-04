ea039c53db35ec52b2190f441117289972124955
Quintin Hull <quintinhull92@gmail.com>
2021-01-04 17:08:00 -0500

    added handle validation error to utils file

--- aurora-project-starter/utils/utils.js
+++ aurora-project-starter/utils/utils.js
@@ -1,4 +1,21 @@
+const { validationResult } = require("express-validator");
+
 const asyncHandler = (handler) => (req, res, next) =>
   handler(req, res, next).catch(next);
 
-module.exports = { asyncHandler };
+const handleValidationErrors = (req, res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors.array().map((error) => error.msg);
+
+    const err = Error("Bad request.");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad request.";
+    return next(err);
+  }
+  next();
+};
+
+module.exports = { asyncHandler, handleValidationErrors };

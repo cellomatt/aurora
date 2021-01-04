34e33ba701639433d499035c873aefdbe8cd8c1a
Quintin Hull <quintinhull92@gmail.com>
2021-01-04 16:57:01 -0500

    worked on sign up route

--- aurora-project-starter/routes/signup.js
+++ aurora-project-starter/routes/signup.js
@@ -1,30 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const csrf = require('csurf');
-const db = require('../db/models');
+const bcrypt = require("bcryptjs");
+const csrf = require("csurf");
+const db = require("../db/models");
 const { User } = db;
+const { asyncHandler } = require("../utils/utils");
 
 const csrfProtection = csrf({ cookie: true });
 router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
 
 /* GET sign-up form. */
-router.get('/', function(req, res, next) {
-  res.render('signup');
+router.get("/", csrfProtection, function (req, res, next) {
+  res.render("signup", { csrfToken: req.csrfToken() });
 });
 
-router.post('/', async function(req, res, next) {
-  const { username, email, password } = req.body;
-  // check if user already exists in db
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({ 
-    username, 
-    email,
-    hashedPassword
-  });
-  
-  res.redirect('/');
-})
+router.post(
+  "/",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    // check if user already exists in db
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword,
+    });
+
+    res.redirect("/");
+  })
+);
 
 module.exports = router;

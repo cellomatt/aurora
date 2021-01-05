c2c7fb2cc1e158b0e9256846aa8105d666349f20
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-05 15:40:15 -0600

    built out question-ask view and functionality to post a question

--- aurora-project-starter/routes/question.js
+++ aurora-project-starter/routes/question.js
@@ -2,28 +2,49 @@ const express = require('express');
 const router = express.Router();
 const csrf = require("csurf");
 const db = require("../db/models");
-const { User, Question, Exerptise } = db;
+const { User, Question, Expertise, Topic } = db;
 const { asyncHandler, handleValidationErrors } = require("../utils/utils");
 const { requireAuth } = require("../utils/auth");
 const csrfProtection = csrf({ cookie: true });
 const { check, validationResult } = require('express-validator');
 router.use(express.urlencoded({ extended: false }));
 
-router.get('/new', requireAuth, csrfProtection, (req, res) => {
+router.get(
+    '/new',
+    requireAuth,
+    csrfProtection,
+    asyncHandler(async (req, res) => {
+    const topics = await Topic.findAll();
+    const expertises = await Expertise.findAll();
 
     res.render('question-ask', {
         title: 'Ask a Question',
-        csrfToken: req.csrfToken()
-
+        topics,
+        expertises,
+        csrfToken: req.csrfToken(),
     })
+}));
 
+router.post(
+    '/',
+    requireAuth,
+    csrfProtection,
+    asyncHandler(async (req, res) => {
+        const { title, topicId, expertiseId, message } = req.body;
+        const { userId } = req.session.auth
+        const topic = await Topic.findByPk(topicId)
 
-});
+        const question = await Question.create({
+            title,
+            message,
+            expertiseId,
+            topicId,
+            userId
+        });
 
-router.post('/', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
-
-    const { userId } = req.session.auth
-
+        res.redirect(`/topics/${topicId}`, {
+            title: `${topic.name}`
+        })
 
 }));
 

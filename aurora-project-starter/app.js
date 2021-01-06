3c8701933d3160d75c3e1753e887b4e6691196d1
Quintin Hull <quintinhull92@gmail.com>
2021-01-06 12:48:20 -0500

    displaying a single answer on answers route

--- aurora-project-starter/app.js
+++ aurora-project-starter/app.js
@@ -17,6 +17,7 @@ const questionsRouter = require("./routes/questions");
 const homeRouter = require("./routes/home");
 const topicsRouter = require("./routes/topics");
 const answersRouter = require("./routes/answers");
+const commentsRouter = require("./routes/comments");
 
 const app = express();
 
@@ -68,7 +69,7 @@ app.use("/questions", questionsRouter);
 app.use("/home", homeRouter);
 app.use("/topics", topicsRouter);
 app.use("/answers", answersRouter);
-
+app.use("/comments", commentsRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- aurora-project-starter/routes/answers.js
+++ aurora-project-starter/routes/answers.js
@@ -1,28 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db/models");
-const { User, Question, Expertise, Topic, Answer } = db;
-const {
-  asyncHandler,
-  csrfProtection
-} = require("../utils/utils");
-const {
-  requireAuth
-} = require("../utils/auth");
+const { User, Question, Expertise, Topic, Answer, Comment } = db;
+const { asyncHandler, csrfProtection } = require("../utils/utils");
+const { requireAuth } = require("../utils/auth");
 
 router.post(
   "/",
   // requireAuth,
   asyncHandler(async (req, res, next) => {
-    const {
-      message,
-      questionId,
-      userId
-    } = req.body;
+    const { message, questionId, userId } = req.body;
     await Answer.create({
       message,
       questionId,
-      userId
+      userId,
     });
 
     //const questions = await Question.findAll({ include: User });
@@ -31,12 +22,25 @@ router.post(
         User,
         Topic,
         Expertise,
-        Answer
-      }
-    })
+        Answer,
+      },
+    });
 
-    return res.redirect(`/questions/${questionId}`)
+    return res.redirect(`/questions/${questionId}`);
+  })
+);
 
+// possibly need route to pull single answer
+router.get(
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const answer = await Answer.findByPk(req.params.id, {
+      includes: { User, Question, Topic, Expertise, Comment },
+    });
+    console.log(answer);
+    return res.render("answer-view", {
+      answer,
+    });
   })
 );
 

d569f28e2cc5b78130aac9ad7ec2597dfbfcfb2c
Antonio <anguilar@aol.com>
2021-01-07 14:36:34 -0800

    Finished main content of home page

--- routes/home.js
+++ routes/home.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const db = require("../db/models");
 const {
     Question,
-    User
+    User,
 } = db;
 const {
     asyncHandler
 } = require('../utils/utils');
 
 /* GET home page. */
-router.get('/', asyncHandler(async (req, res, next) => {
-    const questions = await Question.findAll({ include: [User, Topic, Expertise] });
+router.get('/', asyncHandler(async (req, res) => {
+    const questions = await Question.findAll({ include: User });
 
     res.render('home', {
         title: 'Home',

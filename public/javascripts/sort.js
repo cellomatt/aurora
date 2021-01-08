0f7c2dd3fae720423d79aa83cbfc1b95c615ae70
Max Lettenberger <maxlettenberger@gmail.com>
2021-01-08 11:10:29 -0800

    Sorting done

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -1,18 +1,53 @@
-const filterText = function (id) {
-  let select = document.getElementById(`${id}`);
+document.addEventListener('DOMContentLoaded', ev => {
+  localStorage.clear();
+})
+
+const filterText = id => {
+  let select = document.getElementById(id);
   let option = select.value;
+
+  if (id === 'expertiseSelect') {
+    localStorage.setItem('expertiseSort', option);
+  } else {
+    localStorage.setItem('topicSort', option);
+  }
+
+  let localTopic = localStorage.getItem('topicSort');
+  let localExpertise = localStorage.getItem('expertiseSort');
+
+  if (!localTopic) localStorage.setItem('topicSort', 'All');
+  if (!localExpertise) localStorage.setItem('expertiseSort', 'All');
+
+  localTopic = localStorage.getItem('topicSort');
+  localExpertise = localStorage.getItem('expertiseSort');
+
   let divs = document.querySelectorAll(".result");
-  console.log(divs);
+
   divs.forEach((div) => {
-    if (option === "All") {
-      console.log(option);
-      div.style.display = "inline";
-      return;
-    }
-    if (!div.classList.contains(option)) {
-      div.style.display = "none";
-    } else {
-      div.style.display = "inline";
-    }
+    displayCombinator(localTopic, localExpertise, div);
   });
 };
+
+function displayCombinator(topic, expertise, div) {
+  if (topic === 'All' && expertise === 'All') {
+    div.style.display = 'inline';
+  } else {
+    if (topic === 'All' && expertise !== 'All') {
+      if (div.classList.contains(expertise)) {
+        div.style.display = "inline";
+      } else {
+        div.style.display = "none";
+      }
+    } else if (expertise === 'All' && topic !== 'All') {
+      if (div.classList.contains(topic)) {
+        div.style.display = "inline";
+      } else {
+        div.style.display = "none";
+      }
+    } else if (div.classList.contains(topic) && div.classList.contains(expertise)) {
+      div.style.display = 'inline';
+    } else {
+      div.style.display = 'none';
+    }
+  }
+}

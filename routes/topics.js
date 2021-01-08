181f23f0cd0896dcbe6e535ae46ea8bd484f27b8
Antonio <anguilar@aol.com>
2021-01-08 08:41:59 -0800

    Merging

--- routes/topics.js
+++ routes/topics.js
@@ -5,29 +5,31 @@ const { Topic, Question } = db;
 const { asyncHandler } = require("../utils/utils");
 
 router.get(
-  "/",
-  asyncHandler(async (req, res, next) => {
-    const topics = await Topic.findAll();
-    res.render("topics", {
-      title: "Topics",
-      topics,
-    });
-  })
+    "/",
+    asyncHandler(async (req, res, next) => {
+        const topics = await Topic.findAll();
+        res.render("topics", {
+            title: "Topics",
+            topics,
+        });
+    })
 );
 
 router.get(
-  "/:id",
-  asyncHandler(async (req, res, next) => {
-    const topic = await Topic.findByPk(req.params.id);
-    const questions = await Question.findAll({
-      where: { topicId: req.params.id },
-    });
-    res.render("topic-view", {
-      questions,
-      topic,
-      title: `Topic: ${topic.name}`,
-    });
-  })
+    "/:id",
+    asyncHandler(async (req, res, next) => {
+        const topic = await Topic.findByPk(req.params.id);
+        const questions = await Question.findAll({
+            where: { topicId: req.params.id },
+            include: { include: User },
+        });
+        res.render("topic-view", {
+            questions,
+            topic,
+            title: `Topic: ${topic.name}`,
+        });
+    })
 );
+
 
 module.exports = router;

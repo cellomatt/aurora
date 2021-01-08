5adb96c4089fc5c66178d52bed1b43e249f18b04
Quintin Hull <quintinhull92@gmail.com>
2021-01-08 13:55:07 -0500

    finished topic view page, working on question view page

--- routes/topics.js
+++ routes/topics.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db/models");
-const { Topic, Question } = db;
+const { Topic, Question, User } = db;
 const { asyncHandler } = require("../utils/utils");
 
 router.get(
-    "/",
-    asyncHandler(async (req, res, next) => {
-        const topics = await Topic.findAll();
-        res.render("topics", {
-            title: "Topics",
-            topics,
-        });
-    })
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const topics = await Topic.findAll();
+    res.render("topics", {
+      title: "Topics",
+      topics,
+    });
+  })
 );
 
 router.get(
-    "/:id",
-    asyncHandler(async (req, res, next) => {
-        const topic = await Topic.findByPk(req.params.id);
-        const questions = await Question.findAll({
-            where: { topicId: req.params.id },
-        });
-        res.render("topic-view", {
-            questions,
-            topic,
-            title: `Topic: ${topic.name}`,
-        });
-    })
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const topic = await Topic.findByPk(req.params.id);
+    const questions = await Question.findAll({
+      where: { topicId: req.params.id },
+      include: User,
+    });
+    res.render("topic-view", {
+      questions,
+      topic,
+      title: `Topic: ${topic.name}`,
+    });
+  })
 );
-
 
 module.exports = router;

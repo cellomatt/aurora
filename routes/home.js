732f9fcb660afac4a57b817f3192837473b73710
Antonio <anguilar@aol.com>
2021-01-08 14:34:58 -0800

    Working on delete routing

--- routes/home.js
+++ routes/home.js
@@ -14,12 +14,25 @@ const {
 router.get('/', asyncHandler(async (req, res) => {
     const questions = await Question.findAll({ include: User });
     const topics = await Topic.findAll()
+    // const userId = await auth.userId
+    const uId = req.session.auth.userId
 
     res.render('home', {
         title: 'Home',
         questions,
-        topics
+        topics,
+        uId
     });
+}));
+
+router.post(`/delete/:id`, asyncHandler(async (req, res) => {
+    const question = await Question.findByPk(req.params.id)
+    console.log("---------------------------" + question.title)
+
+    await question.destroy();
+
+
+    res.redirect('/')
 }));
 
 module.exports = router;

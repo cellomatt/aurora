0b5573ba72240d0f4230981c4085c764fce08315
Antonio <anguilar@aol.com>
2021-01-10 12:47:54 -0800

    Fixed the styling on the comments page

--- routes/answers.js
+++ routes/answers.js
@@ -6,61 +6,62 @@ const { asyncHandler, csrfProtection } = require("../utils/utils");
 const { requireAuth } = require("../utils/auth");
 
 router.post(
-  "/",
-  // requireAuth,
-  asyncHandler(async (req, res, next) => {
-    const { message, questionId, userId } = req.body;
-    await Answer.create({
-      message,
-      questionId,
-      userId,
-    });
+    "/",
+    // requireAuth,
+    asyncHandler(async (req, res, next) => {
+        const { message, questionId, userId } = req.body;
+        await Answer.create({
+            message,
+            questionId,
+            userId,
+        });
 
-    //const questions = await Question.findAll({ include: User });
-    const question = await Question.findByPk(req.params.questionId, {
-      includes: {
-        User,
-        Topic,
-        Expertise,
-        Answer,
-      },
-    });
+        //const questions = await Question.findAll({ include: User });
+        const question = await Question.findByPk(req.params.questionId, {
+            includes: {
+                User,
+                Topic,
+                Expertise,
+                Answer,
+            },
+        });
 
-    return res.redirect(`/questions/${questionId}`);
-  })
+        return res.redirect(`/questions/${questionId}`);
+    })
 );
 
 // possibly need route to pull single answer
 router.get(
-  "/:id",
-  csrfProtection,
-  asyncHandler(async (req, res, next) => {
-    let userId;
-    if (req.session.auth) {
-      userId = req.session.auth.userId;
-    } else {
-      userId = 0;
-    }
-    const answer = await Answer.findByPk(req.params.id, {
-      includes: { User, Question, Topic, Expertise, Comment },
-    });
-    const question = await Question.findByPk(answer.questionId);
-    const comments = await Comment.findAll({
-      where: { answerId: req.params.id },
-    });
-    return res.render("answer-view", {
-      title: `Answer: ${answer.message}`,
-      userId,
-      answer,
-      comments,
-      question,
-      csrfToken: req.csrfToken(),
-    });
-  })
+    "/:id",
+    csrfProtection,
+    asyncHandler(async (req, res, next) => {
+        let userId;
+        if (req.session.auth) {
+            userId = req.session.auth.userId;
+        } else {
+            userId = 0;
+        }
+        const answer = await Answer.findByPk(req.params.id, {
+            includes: { User, Question, Topic, Expertise, Comment },
+        });
+        const question = await Question.findByPk(answer.questionId);
+        const comments = await Comment.findAll({
+            where: { answerId: req.params.id },
+            include: User
+        });
+        return res.render("answer-view", {
+            title: `Answer: ${answer.message}`,
+            userId,
+            answer,
+            comments,
+            question,
+            csrfToken: req.csrfToken(),
+        });
+    })
 );
 
 router.get(`/:id/delete`, asyncHandler(async (req, res) => {
-  const answer = await Answer.findByPk(req.params.id)
-  await answer.destroy();
+    const answer = await Answer.findByPk(req.params.id)
+    await answer.destroy();
 }));
 module.exports = router;
--- routes/comments.js
+++ routes/comments.js
@@ -7,19 +7,19 @@ const { asyncHandler, csrfProtection } = require("../utils/utils");
 const { requireAuth } = require("../utils/auth");
 
 router.post(
-  "/",
-  // requireAuth,
-  asyncHandler(async (req, res, next) => {
-    const { commentMessage, answerId, userId } = req.body;
+    "/",
+    // requireAuth,
+    asyncHandler(async (req, res, next) => {
+        const { commentMessage, answerId, userId } = req.body;
 
-    const comment = await Comment.create({
-      message: commentMessage,
-      answerId,
-      userId,
-    });
+        const comment = await Comment.create({
+            message: commentMessage,
+            answerId,
+            userId,
+        });
 
-    return res.redirect(`/answers/${answerId}`);
-  })
+        return res.redirect(`/answers/${answerId}`);
+    })
 );
 
 // router.get(
@@ -49,8 +49,8 @@ router.post(
 // );
 
 router.get(`/:id/delete`, asyncHandler(async (req, res) => {
-  const comment = await Comment.findByPk(req.params.id)
-  await comment.destroy();
+    const comment = await Comment.findByPk(req.params.id)
+    await comment.destroy();
 }));
 
 module.exports = router;
